769afde34bcb595758da00e174b7e2aed7046b26
zixinglin07 <92582985+zixinglin07@users.noreply.github.com>
2023-08-09 09:34:56 +0800

    added clip into test_refcoco

--- test_refcoco.py
+++ test_refcoco.py
@@ -471,7 +471,7 @@ def bbox_instancing(bboxes,segmaps,min_size = 5):
         while(i<len(instanced_boxes)):
             x_contiguous,y_contiguous = contiguous_transparency_scan(instanced_boxes[i],segmap,instance_threshold=0)
             #x_contiguous,y_contiguous = segmap_instancing(instanced_boxes[i],segmap,instance_threshold=0)
-            print (x_contiguous)
+            #print (x_contiguous)
             new_bboxes = splitBox(instanced_boxes[i],x_contiguous,y_contiguous,segmap)
             #print(new_bboxes)
             if new_bboxes != None:
@@ -492,7 +492,7 @@ def bbox_instancing(bboxes,segmaps,min_size = 5):
 
             sim = boxSimilarity(instanced_boxes[i],instanced_boxes[i+1])
             if (sim<5): #too similar
-                print(instanced_boxes[i],instanced_boxes[i+1])
+                #print(instanced_boxes[i],instanced_boxes[i+1])
                 # remove smaller box
                 if boxSize(instanced_boxes[i])>boxSize(instanced_boxes[i+1]):
                     instanced_boxes.pop(i+1)
@@ -575,6 +575,11 @@ evaluator.eval()
 
 #initialize CLIP
 
+import clip
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
 
 for ref_id in ref_ids:
     ref = refer.loadRefs(ref_id)[0]
@@ -611,7 +616,7 @@ for ref_id in ref_ids:
     image = transform(image).unsqueeze(0)
     img = image[0].permute(1,2,0)
     img = img * 0.5 + 0.5
-    plt.imshow(img)
+    #plt.imshow(img)
     
     
     with torch.no_grad():
@@ -631,11 +636,11 @@ for ref_id in ref_ids:
     img = Image.fromarray(np.uint8(255*img)).convert("RGBA")
     seg = mask.convert("RGBA")
     out = Image.blend(img, seg, alpha)
-    plt.axis('off')
+    #plt.axis('off')
     #plt.imshow(img)
-    plt.figure()
-    plt.legend(handles=patches, loc='upper right', bbox_to_anchor=(1.5, 1), prop={'size': 20})
-    plt.axis('off')
+    #plt.figure()
+    #plt.legend(handles=patches, loc='upper right', bbox_to_anchor=(1.5, 1), prop={'size': 20})
+    #plt.axis('off')
     #plt.imshow(seg)
     
     #get bbox
@@ -645,12 +650,12 @@ for ref_id in ref_ids:
     #Show results of bounding boxes
     from PIL import Image, ImageDraw
     img_copy = img.copy()
-    for i in range(0,len(bboxes)-1): #the other BBOX got bug, so minus 1
-        topLeft = (bboxes[i][0],bboxes[i][1])
-        bottomRight = (bboxes[i][2],bboxes[i][3])
-        img1 = ImageDraw.Draw(img_copy)
-        #rect_color = getHexColor(rgba_cols[i])
-        draw_rectangle(img1, (topLeft,bottomRight),color = tuple(rgba_cols[i]), width=5)
+    #for i in range(0,len(bboxes)-1):
+    #    topLeft = (bboxes[i][0],bboxes[i][1])
+    #    bottomRight = (bboxes[i][2],bboxes[i][3])
+    #    img1 = ImageDraw.Draw(img_copy)
+    #    rect_color = getHexColor(rgba_cols[i])
+    #    draw_rectangle(img1, (topLeft,bottomRight),color = tuple(rgba_cols[i]), width=5)
     #plt.legend(handles=patches, loc='upper right', bbox_to_anchor=(1.5, 1), prop={'size': 20})
     #plt.axis('off')
     #plt.imshow(img_copy)
@@ -660,28 +665,112 @@ for ref_id in ref_ids:
     new_bbox = bbox_instancing(bboxes,segmaps,min_size=min_size) 
     
     #sample bbox instancing result
-    from PIL import Image, ImageDraw
     img_copy_test = img.copy()
-    for i in range(len(new_bbox)):
+    for i in range(len(new_bbox)-1):
         for j in range(len(new_bbox[i])):
             topLeft = (new_bbox[i][j][0],new_bbox[i][j][1])
             bottomRight = (new_bbox[i][j][2],new_bbox[i][j][3])
             img1 = ImageDraw.Draw(img_copy_test)
             #rect_color = getHexColor(rgba_cols[i])
-            draw_rectangle(img1, (topLeft,bottomRight),color = tuple(rgba_cols[i]), width=2)
+            draw_rectangle(img1, (topLeft,bottomRight),color = "red", width=5)
             #draw_rectangle(img1, (topLeft,bottomRight),color = "red", width=2)
     #plt.legend(handles=patches, loc='upper right', bbox_to_anchor=(1.5, 1), prop={'size': 20})
     plt.axis('off')
     plt.imshow(img_copy_test)
-
+    
     #*******TODO****
-    #extract bounding boxes
     #if only 1 bounding box, just return that one
-    print('pass to clip:')
-    for sentence in ref['sentences']:
-        print (sentence['sent'])
+    best_bbox = -1
+    if len(new_bbox[0])==1:
+        #return that one box
+        best_bbox = 0
+    else:
+        crop_size = 480
+        padding = [0.0] * 3
+        img_copy_clip = Image.open(img_path+refer.loadImgs(image_id)[0]['file_name'])
+        print(type(img_copy_clip))
+        img_copy_clip = np.array(img_copy_clip)
+        #transform = transforms.Compose(
+        #    [
+        #        transforms.ToTensor(),
+        #        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+        #    ]
+        #)
+        #image = transform(image).unsqueeze(0)
+        #img = image[0].permute(1,2,0)
+        #img = img * 0.5 + 0.5
+        #plt.imshow(img)
+
+
+
+        cropped_imgs = []
+        for class_bbox in new_bbox:
+            cropped_imgs.append([])
+            for bbox in class_bbox:
+                cropped_imgs[len(cropped_imgs)-1].append(img_copy_clip[bbox[1]:bbox[3],bbox[0]:bbox[2]])
+
+
+
+        for cls in cropped_imgs:
+            for i in range(len(cls)):
+                #img_transform = transform(cls[i]).unsqueeze(0)
+                #print(cls[i])
+                cls[i]=Image.fromarray(cls[i])
+            
+        
+        vote_arr = [0]*len(new_bbox[0])
+
+        for sentence in ref['sentences']:          
+            max_score = 0
+            best_id = -1
+            ret_index_temp = []
+            ret_index_cls = []
+            print("number of cropped: "+ str(len(cropped_imgs[0])))
+            for j in range(len(cropped_imgs[0])): #for each cropped_image run it through clip with the label of description and get the highest scoring boxes
+                #image = preprocess(Image.open("CLIP.png")).unsqueeze(0).to(device)
+                image = preprocess(cropped_imgs[0][j]).unsqueeze(0).to(device)
+                #text = clip.tokenize(["person in blue", "person in white", "person in red"]).to(device)
+                text = clip.tokenize([sentence['sent'], "other"]).to(device)  #extracted input is also based on class
+                with torch.no_grad():
+                    image_features = model.encode_image(image)
+                    text_features = model.encode_text(text)
+
+                    logits_per_image, logits_per_text = model(image, text)
+                    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+                print("Box "+str(j)+": "+str(probs[0][0]))
+                #if score is higher than max, max = score and append to list. if score within 0.1 of max score, append as well
+                if probs[0][0]>max_score:
+                    max_score = probs[0][0]
+                    best_id = j
+            vote_arr[best_id]+=1
+        
+        print(vote_arr)
+        best_bbox = vote_arr.index(max(vote_arr))
+        
+    plt.imshow(cropped_imgs[0][best_bbox])    
+    print(new_bbox[0][best_bbox])
+                #probs[0][0]
+    #print('pass to clip:')
+    #for sentence in ref['sentences']:
+    #    print (sentence['sent'])
     #run clip with 3 prompts, use voting to determine which to return. only return 1 bbox for each prompts
     
+    #draw results from clip
+    from PIL import Image, ImageDraw
+    img_copy_test2 = img.copy()
+    print("clip retval: " + str(best_bbox))
+    #print((processed_new_bbox[i][j]))
+    topLeft = (new_bbox[0][best_bbox][0],new_bbox[0][best_bbox][1])
+    bottomRight = (new_bbox[0][best_bbox][2],new_bbox[0][best_bbox][3])
+    img2 = ImageDraw.Draw(img_copy_test2)
+    #rect_color = getHexColor(rgba_cols[i])
+    draw_rectangle(img2, (topLeft,bottomRight),color = tuple(rgba_cols[i]), width=8)
+    #draw_rectangle(img1, (topLeft,bottomRight),color = "red", width=2)
+    plt.axis('off')
+    plt.imshow(img_copy_test2)
+    plt.legend(handles=patches, loc='upper right', bbox_to_anchor=(1.5, 1), prop={'size': 20})
+    #**TODO** IOU, remember to check for size difference
     plt.figure()
     refer.showRef(ref, seg_box='box')
     plt.show()
