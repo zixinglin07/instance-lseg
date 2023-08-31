046b8a563d53ae670d3ac5571bbf0da2a3c7e804
zixinglin07 <92582985+zixinglin07@users.noreply.github.com>
2023-08-31 16:08:43 +0800

    format code slightly

---
+++ run_mproc.bat
@@ -0,0 +1,5 @@
+call C:/Users/bater/anaconda3/Scripts/activate.bat C:/Users/bater/anaconda3
+call cd "C:\Users\bater\OneDrive - Nanyang Technological University\2023 Jan Sem\FYP\Github\zero-shot-referring-with-natural-language"
+call conda activate lseg37
+call python refcoco_mproc.py
+pause
